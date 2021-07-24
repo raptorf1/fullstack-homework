e877e0de0ef84f44458e6106d4fbcc70b2711f7b
raptorf1 <raptor_f1@hotmail.com>
2021-07-24 11:42:43 +0300

    Calculates and sends humus_balance for existing fields

--- soil-service/app/controllers/fields_controller.rb
+++ soil-service/app/controllers/fields_controller.rb
@@ -2,8 +2,12 @@ class FieldsController < ActionController::Base
   def index
     all_fields = FieldsService.instance.fetch_fields
     all_fields.each do |field|
-      binding.pry
+      field_deltas = []
+      field[:crops].each do |crop|
+        field_deltas.push(crop[:crop][:humus_delta])
+      end
+      field.store(:humus_balance, field_deltas.sum)
     end
-    render json: FieldsService.instance.fetch_fields
+    render json: all_fields
   end
 end
