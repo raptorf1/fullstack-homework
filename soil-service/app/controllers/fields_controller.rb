eee7e2e9cf3704e5411b5acc225892e814371953
raptorf1 <raptor_f1@hotmail.com>
2021-07-24 11:05:28 +0300

    Installs pry-rails gem. Starts iterating through Fields to calculate humus delta.

--- soil-service/app/controllers/fields_controller.rb
+++ soil-service/app/controllers/fields_controller.rb
@@ -1,5 +1,9 @@
 class FieldsController < ActionController::Base
   def index
+    all_fields = FieldsService.instance.fetch_fields
+    all_fields.each do |field|
+      binding.pry
+    end
     render json: FieldsService.instance.fetch_fields
   end
 end
